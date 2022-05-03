7b4a68095fecd279358d7737d9944846e7f203d8
Markus Wiland <markus.wiland@outlook.com>
2022-05-03 09:28:50 +0200

    second commit

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -7,7 +7,7 @@
     >
       <div class="character-inner">
         <div class="character-front">
-          <img :src="character.img" class="character_image" />
+          <img :src="character.img" class="character-image" />
         </div>
         <div class="character-back">
           <h1>{{ character.name }}</h1>
@@ -33,13 +33,42 @@ export default {
 <style scoped>
 .characters {
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   grid-gap: 0.5rem;
 }
+.character {
+  perspective: 1000px;
+  background-color: transparent;
+  width: 100%;
+  height: 250px;
+}
+.character-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
 
-.character_image {
+.character-front,
+.character-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+.character:hover .character-inner {
+  transform: rotateY(180deg);
+}
+.character-image {
   width: 100%;
   height: 250px;
   object-fit: cover;
+}
+.character-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
 }
 </style>
